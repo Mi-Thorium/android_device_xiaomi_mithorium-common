249cbf1829ea57eb1d2dba40f1aefb353605a1ca
Michael Bestas <mkbestas@lineageos.org>
2020-11-24 00:34:40 +0200

    msm8937-common: Move telephony related packages to /system_ext
    Change-Id: I6819c2af9f0f6e0e3823b853277287fc5aa8d504

--- msm8937.mk
+++ msm8937.mk
@@ -366,7 +366,7 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/telephony_system_ext_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system_ext_privapp-permissions-qti.xml
 
 # Wifi
 PRODUCT_PACKAGES += \
