9f3549b226e737cc5a30e360ff357b40aa7eaeff
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-09 14:53:21 +0800

    msm8937-common: rootdir: Improve baseband detection method
    Co-authored-by: Pranaya Deomani <deomanipranaya007@gmail.com>

--- rootdir/bin/init.baseband.sh
+++ rootdir/bin/init.baseband.sh
@@ -1,6 +1,6 @@
 #!/vendor/bin/sh
 
-baseband_str=$(strings /vendor/firmware_mnt/image/modem.b12 | grep "MPSS.*_V" | head -1)
+baseband_str=$(strings /vendor/firmware_mnt/image/modem.b12 | grep "QC_IMAGE_VERSION_STRING=" | cut -c 25-)
 
 if [ ! -z $baseband_str ]; then
     setprop gsm.version.baseband $baseband_str
