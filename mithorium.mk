50be642e978859759db35a577c2253d93532ce19
LuK1337 <priv.luk@gmail.com>
2021-12-15 17:58:52 +0100

    mithorium-common: RIP com.dsi.ant.antradio_library
    No longer shipped by default with lineage-19.0.
    
    Change-Id: Iae740ef18505b7f2bb01fe54d45810e72fd521e9

--- mithorium.mk
+++ mithorium.mk
@@ -24,7 +24,6 @@ PRODUCT_AAPT_PREF_CONFIG ?= xhdpi
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
@@ -65,8 +64,7 @@ endif
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant@1.0.vendor \
-    com.dsi.ant.antradio_library
+    com.dsi.ant@1.0.vendor
 
 # Audio
 PRODUCT_PACKAGES += \
