af3b525a2140b3968fc24708e9a6c58b1e524c30
Etienne Ruffieux <eruffieux@google.com>
2022-01-20 21:18:07 +0000

    mithorium-common: Added new vendors for Bluetooth config migration
    Tag: #feature
    Test: manual
    Bug: 211570675
    Change-Id: Ib69dd91027c9579f2fc12099d973c993f2c073cd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -139,6 +139,7 @@ TARGET_ENFORCES_QSSI := true
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
