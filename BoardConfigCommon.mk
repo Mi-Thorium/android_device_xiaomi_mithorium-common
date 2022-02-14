49f87f9684af2a337f4e372882d2c70265fe544a
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-02-14 17:51:24 +0800

    msm8937-common: libinit: Allow devices to use theirs own init

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,8 +111,8 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):init_xiaomi_msm8937
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_msm8937
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_msm8937
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_msm8937
 
 # Partitions
 TARGET_COPY_OUT_VENDOR := vendor
