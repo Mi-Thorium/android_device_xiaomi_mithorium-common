cb6a44e4139f3b76c519c0cc30235f44def79386
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-02-18 14:20:56 +0800

    EXTRA: msm8937-common: Inherit private extras if exists

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -183,3 +183,6 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 include vendor/xiaomi/mithorium-common/BoardConfigVendor.mk
+
+# Inherit private extras
+-include vendor/extras/board.mk
