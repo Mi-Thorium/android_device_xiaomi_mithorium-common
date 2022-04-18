c90502ad002470958b3818ce97c6d70b61305987
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-18 17:29:29 +0800

    fixup! msm8937-common: Move telephony related packages to /system_ext

--- extract-files.sh
+++ extract-files.sh
@@ -66,8 +66,16 @@ function blob_fixup() {
         system_ext/etc/init/dpmd.rc)
             sed -i "s|/system/product/bin/|/system/system_ext/bin/|g" "${2}"
             ;;
-        system_ext/etc/permissions/com.qti.dpmframework.xml | system_ext/etc/permissions/dpmapi.xml | system_ext/etc/permissions/telephonyservice.xml)
+        system_ext/etc/permissions/com.qti.dpmframework.xml \
+        | system_ext/etc/permissions/com.qualcomm.qti.imscmservice-V2.0-java.xml \
+        | system_ext/etc/permissions/com.qualcomm.qti.imscmservice-V2.1-java.xml \
+        | system_ext/etc/permissions/com.qualcomm.qti.imscmservice-V2.2-java.xml \
+        | system_ext/etc/permissions/dpmapi.xml \
+        | system_ext/etc/permissions/telephonyservice.xml )
             sed -i "s|/system/product/framework/|/system/system_ext/framework/|g" "${2}"
+            ;;
+        system_ext/etc/permissions/embms.xml)
+            sed -i "s|/product/framework/|/system_ext/framework/|g" "${2}"
             ;;
         system_ext/etc/permissions/qcrilhook.xml)
             sed -i 's|/product/framework/qcrilhook.jar|/system_ext/framework/qcrilhook.jar|g' "${2}"
