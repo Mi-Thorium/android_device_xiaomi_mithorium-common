47a2c23b42c12bb864c943be422a9f93822a53df
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-18 19:45:31 +0800

    msm8937-common: Build required libraries for lib-imscamera.so

--- msm8937.mk
+++ msm8937.mk
@@ -201,6 +201,10 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.3 \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.provider@2.4 \
+    android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0
 
 # HW crypto
