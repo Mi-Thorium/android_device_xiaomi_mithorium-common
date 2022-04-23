e62422e8e800d889bf4acdca181add004e8acd2e
me-cafebabe <me_cafebabe@hotmail.com>
2022-04-23 20:05:01 +0800

    msm8937-common: Clear current qc blobs list and fixups

--- extract-files.sh
+++ extract-files.sh
@@ -58,52 +58,6 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-function blob_fixup() {
-    case "${1}" in
-        product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
-            sed -i 's/version="2.0"/version="1.0"/g' "${2}"
-            ;;
-        system_ext/etc/init/dpmd.rc)
-            sed -i "s|/system/product/bin/|/system/system_ext/bin/|g" "${2}"
-            ;;
-        system_ext/etc/permissions/com.qti.dpmframework.xml \
-        | system_ext/etc/permissions/com.qualcomm.qti.imscmservice-V2.0-java.xml \
-        | system_ext/etc/permissions/com.qualcomm.qti.imscmservice-V2.1-java.xml \
-        | system_ext/etc/permissions/com.qualcomm.qti.imscmservice-V2.2-java.xml \
-        | system_ext/etc/permissions/dpmapi.xml \
-        | system_ext/etc/permissions/telephonyservice.xml )
-            sed -i "s|/system/product/framework/|/system/system_ext/framework/|g" "${2}"
-            ;;
-        system_ext/etc/permissions/embms.xml)
-            sed -i "s|/product/framework/|/system_ext/framework/|g" "${2}"
-            ;;
-        system_ext/etc/permissions/qcrilhook.xml)
-            sed -i 's|/product/framework/qcrilhook.jar|/system_ext/framework/qcrilhook.jar|g' "${2}"
-            ;;
-        system_ext/lib64/libdpmframework.so)
-            for LIBSHIM_DPMFRAMEWORK in $(grep -L "libshim_dpmframework.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libshim_dpmframework.so" "${2}"
-            done
-            ;;
-        vendor/lib/mediadrm/libwvdrmengine.so|vendor/lib64/mediadrm/libwvdrmengine.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
-            ;;
-        vendor/lib64/libsettings.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
-            ;;
-        vendor/lib64/libwvhidl.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
-            ;;
-        vendor/lib64/libril-qc-hal-qmi.so)
-                "${PATCHELF}" --replace-needed "android.hardware.radio.config@1.1.so" "android.hardware.radio.config@1.1_shim.so" "${2}"
-        ;;
-        vendor/lib64/android.hardware.radio.config@1.1_shim.so)
-                "${PATCHELF}" --set-soname "android.hardware.radio.config@1.1_shim.so" "${2}"
-                sed -i -e 's|android.hardware.radio.config@1.1::IRadioConfig\x00|android.hardware.radio.config@1.0::IRadioConfig\x00|g' "${2}"
-        ;;
-    esac
-}
-
 if [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper for common device
     setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
