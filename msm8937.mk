ff2368da896a79d099c75d2f627a6be662a3452b
me-cafebabe <me_cafebabe@hotmail.com>
2022-04-26 00:37:28 +0800

    msm8937-common: Build libavservices_minijail.vendor
    New blobs requires this

--- msm8937.mk
+++ msm8937.mk
@@ -254,6 +254,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+PRODUCT_PACKAGES += libavservices_minijail.vendor
+
 # MSM IRQ Balancer
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
