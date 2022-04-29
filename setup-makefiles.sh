ef3252efd1cfe9bc967333192836200e91d56041
me-cafebabe <me_cafebabe@hotmail.com>
2022-04-29 20:24:17 +0800

    mithorium-common: New list of supported devices
    Yeah, The first character on unified codenames has been converted to uppercase, and added two msm8953 devices

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "land riva rolex santoni tiare ulysse"
+write_headers "Mi8937 Mi439 Tiare uter vince"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
