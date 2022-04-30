80d42a652059f69671a4c86a8b141171132a05f6
me-cafebabe <me_cafebabe@hotmail.com>
2022-04-30 21:58:54 +0800

    mithorium-common: Use QTI Vibrator AIDL

--- mithorium.mk
+++ mithorium.mk
@@ -377,11 +377,8 @@ PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
 # Vibrator
-ifneq ($(TARGET_USES_DEVICE_SPECIFIC_VIBRATOR),true)
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
-endif
+    vendor.qti.hardware.vibrator.service
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
