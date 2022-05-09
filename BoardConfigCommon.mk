fb703b86694d349251313c65b42d6af4e01f9788
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-09 19:38:07 +0800

    mithorium-common: BOARD_PLAT_PRIVATE_SEPOLICY_DIR => SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -181,8 +181,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 ifeq (true,$(call math_lt,$(PRODUCT_SHIPPING_API_LEVEL),28))
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/legacy/vendor
 endif
