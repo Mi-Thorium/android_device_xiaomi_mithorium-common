6740c7273a2f50b3ec3e2f8b331be0ccf8971ccc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:18:55 +0200

    mithorium-common: Drop QTI camera device usage
    * qcom-camera topic hasn't been ported to 19.1
    * Keep building vendor.qti.hardware.camera.device@1.0 interface lib, IMS stack and possibly camera HAL still needs it
    
    Change-Id: I87bcd5b54ad986d5844df50de243fc1a18507198

--- mithorium.mk
+++ mithorium.mk
@@ -148,6 +148,9 @@ MITHORIUM_PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service
 
 MITHORIUM_PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.device@1.0.vendor
+
+MITHORIUM_PRODUCT_PACKAGES += \
     libstdc++.vendor
 
 # Configstore
