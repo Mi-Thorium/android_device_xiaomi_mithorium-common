0dc42ca015f0a40867faaed7d7960906b71ed1b1
Sri19944 <89214859+Sri19944@users.noreply.github.com>
2022-08-01 13:31:49 +0530

    Update LocApiBase.h
    (uint32_t left,
                uint32_t center, uint32_t right, uint8_t gloFrequency)

--- gps/core/LocApiBase.h
+++ gps/core/LocApiBase.h
@@ -92,7 +92,8 @@ public:
     inline LocApiProxyBase() {}
     inline virtual ~LocApiProxyBase() {}
     inline virtual void* getSibling2() { return NULL; }
-    inline virtual double getGloRfLoss()
+    inline virtual double getGloRfLoss(uint32_t left,
+            uint32_t center, uint32_t right, uint8_t gloFrequency)
                                        { return 0.0; }
 };
 
