267b56e3c228eb181a3a8d79f984646355bee790
dextyy5 <111626839+dextyy5@users.noreply.github.com>
2022-08-20 11:25:33 +0530

    Create Android.mk

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,55 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    AmbientSensePrebuilt \
+    arcore \
+    Camera2 \
+    Chrome \
+    Chrome-Stub \
+    ConnMO \
+    ConnMetrics \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DMService \
+    Drive \
+    GCS \
+    GoogleCamera \
+    Maps \
+    Music \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OdadPrebuilt \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    PixelLiveWallpaperPrebuilt \
+    Photos \
+    PlayAutoInstallConfig \
+    PrebuiltGmail \
+    RecorderPrebuilt \
+    SafetyHubPrebuilt \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    Snap2 \
+    SnapdragonCamera \
+    SoundAmplifierPrebuilt \
+    SprintDM \
+    SprintHM \
+    Tycho \
+    USCCDM \
+    Videos \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    YouTube \
+    YouTubeMusicPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
