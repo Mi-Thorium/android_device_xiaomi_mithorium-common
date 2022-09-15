79326dfe9712ab63075e8ecee9a512fea6902315
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-15 20:49:00 +0800

    mithorium-common: rootdir: Copy init scripts
    (Edit on 2023/01/06: Remove init.qti.dcvs.sh as it's only used on k4.19)

--- mithorium.mk
+++ mithorium.mk
@@ -326,9 +326,21 @@ PRODUCT_COPY_FILES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.qcom.rc \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
-    init.xiaomi.rc
+    init.target.rc \
+    init.xiaomi.rc \
+    ueventd.qcom.rc
+
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.qseecomd.sh
 
 # RenderScript
 PRODUCT_PACKAGES += \
