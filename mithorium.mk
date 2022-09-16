9e40c9ae00ca61a1d5c42a84e8b5426c99803271
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-17 01:04:15 +0800

    mithorium-common: k4.19: Update blobs to LA.UM.9.6.3.r1-06200-89xx.0

--- mithorium.mk
+++ mithorium.mk
@@ -129,9 +129,16 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
+
+ifeq ($(TARGET_KERNEL_VERSION),4.9)
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+else ifeq ($(TARGET_KERNEL_VERSION),4.19)
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor
+endif
 
 # Camera
 PRODUCT_PACKAGES += \
