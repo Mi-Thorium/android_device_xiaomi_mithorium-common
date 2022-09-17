8ccbeec9423d30873ea03024c1f68d083c16c770
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-18 01:37:57 +0800

    mithorium-common: Build qti display mapper HIDL libs
    LA.UM.9.6.3 blobs needs that

--- mithorium.mk
+++ mithorium.mk
@@ -167,6 +167,10 @@ PRODUCT_PACKAGES += \
 ifeq ($(TARGET_KERNEL_VERSION),4.19)
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.display.mapperextensions@1.1.vendor \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display
 else
