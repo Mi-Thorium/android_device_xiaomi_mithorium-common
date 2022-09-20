b8cd0a0f41df8158f14d0d4247b5b3f7ac802f75
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-20 16:19:00 +0800

    Revert "mithorium-common: Include device FCM from vendor/lineage"
    This reverts commit 9f9483964cc8d688bd58f2aaedfc6950c45fca20.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -165,9 +165,7 @@ endif # TARGET_USES_MITHORIUM_KERNEL
 BOARD_SHIPPING_API_LEVEL := 30
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_k$(TARGET_KERNEL_VERSION).xml
