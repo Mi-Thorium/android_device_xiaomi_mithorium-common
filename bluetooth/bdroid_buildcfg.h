9e3bb80bb5e6e45259bd0c87f8e022f630384dd4
Michael Bestas <mkbestas@lineageos.org>
2022-09-29 12:35:43 +0300

    mithorium-common: Remove bdroid_buildcfg.h
    As of I3c7aed1bdb45db728d319f7d757400e44d232283 the Bluetooth stack
    defaults to reasonable defaults that should work for all devices.
    
    [cafebabe] Keep BTM_SCO_ENHANCED_SYNC_ENABLED flag
    
    Change-Id: I056d63c4c8a2727e384172e964dc0000b6773504

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -16,17 +16,5 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_ACL_CONNECTIONS    7
-#define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
-
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BTM_WBS_INCLUDED TRUE
-#define BTIF_HF_WBS_PREFERRED TRUE
 #endif
