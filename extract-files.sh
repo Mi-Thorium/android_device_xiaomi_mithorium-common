8ec4cf96cd1beade6805250ef147dd2b058cec8e
me-cafebabe <me_cafebabe@hotmail.com>
2022-12-21 16:04:08 +0800

    mithorium-common: k4.9: Shift to LA.UM.9.6.3.r1-06200-89xx.0 blobs

--- extract-files.sh
+++ extract-files.sh
@@ -77,19 +77,7 @@ function blob_fixup() {
         | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
             sed -i 's|version="2.0"|version="1.0"|g' "${2}"
             ;;
-        vendor/lib64/libQmiservices.so | vendor/lib64/libril-qc-hal-qmi.so )
-            sed -i 's|libqmiservices.so|libQmiservices.so|g' "${2}"
-            ;;
     esac
-
-    if [ "${KERNEL_419}" != "true" ]; then
-        # Kernel 4.9
-        case "${1}" in
-            vendor/lib64/libQmiservices.so | vendor/lib64/libril-qc-hal-qmi.so )
-                sed -i 's|libqmiservices.so|libQmiservices.so|g' "${2}"
-                ;;
-        esac
-    fi
 }
 
 if [ -z "${ONLY_TARGET}" ]; then
