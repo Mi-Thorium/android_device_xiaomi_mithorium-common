957d05432aed7657bd56a1ddf79d3665be4450d9
me-cafebabe <me_cafebabe@hotmail.com>
2022-12-23 19:19:25 +0800

    mithorium-common: Build camera HAL in device specific tree

--- mithorium.mk
+++ mithorium.mk
@@ -122,9 +122,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    camera.msm8937 \
-    libmm-qcamera
+    android.hardware.camera.provider@2.4-service
 
 PRODUCT_PACKAGES += \
     libstdc++.vendor
