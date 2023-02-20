58118815536fc867c374703f075f56af673d53b8
me-cafebabe <me.cafebabe@gmail.com>
2023-02-21 04:15:19 +0800

    mithorium-common: configs: Import msm_irqbalance_little_big from FP3

--- mithorium.mk
+++ mithorium.mk
@@ -329,7 +329,8 @@ MITHORIUM_PRODUCT_PACKAGES += libavservices_minijail.vendor
 
 # MSM IRQ Balancer
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
+    $(LOCAL_PATH)/configs/msm_irqbalance_little_big.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_little_big.conf
 
 # OMX
 MITHORIUM_PRODUCT_PACKAGES += \
