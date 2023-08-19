a97719aaee27e9db6a7236271e2cd02224367157
me-cafebabe <me.cafebabe@gmail.com>
2023-08-19 20:06:17 +0800

    mithorium-common: Move to new MiThorium QCOM HALs structure
    Change-Id: I445b5a764e72ffbab0f492e71a14f9ae56270059

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -186,13 +186,7 @@ TARGET_USES_INTERACTION_BOOST := true
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
-ifeq ($(USE_MITHORIUM_HALS),true)
-    ifeq ($(TARGET_USES_Q_DISPLAY_STACK),true)
-        QCOM_SOONG_NAMESPACE := hardware/mithorium-$(TARGET_KERNEL_VERSION)-q-display
-    else
-        QCOM_SOONG_NAMESPACE := hardware/mithorium-$(TARGET_KERNEL_VERSION)
-    endif
-else
+ifneq ($(USE_MITHORIUM_QCOM_HALS),true)
 TARGET_ENFORCE_QSSI := true
 endif
 
