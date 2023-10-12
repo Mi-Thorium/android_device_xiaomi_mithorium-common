505e1465fcb4923e5ae490a070282bf2e162c716
Chippa-a <bagirovvusal99@gmail.com>
2023-10-12 22:24:19 +0300

    mithorium-common: charger_led: Provide own rc file for recovery
    [cafebabe: Rename to charger_led_recovery, Move LED chowns to here]
    
    Change-Id: Ib999d5e3accf7b6a1f2e3f45df4c0dbab81cc162

--- mithorium.mk
+++ mithorium.mk
@@ -161,7 +161,7 @@ MITHORIUM_PRODUCT_PACKAGES += \
 # Charger
 MITHORIUM_PRODUCT_PACKAGES += \
     charger_led \
-    charger_led.recovery
+    charger_led_recovery
 
 # Configstore
 ifeq ($(TARGET_KERNEL_VERSION),4.19)
