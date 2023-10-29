da93989267147c3672a0e294f6e3e098eb2ae68c
Yumi Yukimura <me.cafebabe@gmail.com>
2023-10-30 03:26:00 +0800

    [DNM] debug offline charging mode
    Change-Id: Ic5b9f58a2f1471705ad59ffbee7923cd966e2ba7

--- libinit/init_xiaomi_mithorium.cpp
+++ libinit/init_xiaomi_mithorium.cpp
@@ -5,9 +5,16 @@
  */
 
 #include <libinit_dalvik_heap.h>
+#include <libinit_utils.h>
 
 #include "vendor_init.h"
 
 void vendor_load_properties() {
     set_dalvik_heap();
+
+    property_override("ro.secure", "0");
+    property_override("ro.adb.secure", "0");
+    property_override("ro.debuggable", "1");
+    property_override("sys.usb.config", "adb");
+    property_override("persist.sys.usb.config", "adb");
 }
