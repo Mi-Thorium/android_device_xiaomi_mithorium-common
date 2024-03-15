91db1bc33ba53dd58ba7e24e9be3276ca3d6d4c7
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-15 15:17:25 +0800

    Revert "mithorium-common: Switch to legacy WiFi HIDL HAL"
    * WiFi AIDL HAL works in U QPR2
      (also, the legacy WiFi HIDL HAL has stopped working, lol)
    
    This reverts commit 6c57054d35914fe9eddb3fa1fc9a6a72fc158dda.
    
    Change-Id: I4d38fcc7e5999f934344bc364afb4f4d8a049711

--- mithorium.mk
+++ mithorium.mk
@@ -574,7 +574,7 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 MITHORIUM_PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy \
+    android.hardware.wifi-service \
     libcld80211 \
     libwifi-hal-ctrl \
     libwpa_client \
