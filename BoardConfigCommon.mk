5776a917d9d077607f86d91d53eeceffd283ba5d
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-16 04:14:55 +0800

    mithorium-common: Switch to IR AIDL HAL
    Change-Id: I23dfb8226c722a3ed3baf593c916234388580da8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,9 +172,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_k$(TARGET_KERNEL_VERSION).xml
-ifneq ($(TARGET_HAS_NO_CONSUMERIR),true)
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest/consumerir.xml
-endif
 ifneq ($(TARGET_HAS_NO_RADIO),true)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest/radio.xml
 endif
