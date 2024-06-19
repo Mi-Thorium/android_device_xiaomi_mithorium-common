43f35d40d1d9b4ecafbe0742dfd8bba057d3fc84
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-19 12:28:11 +0800

    mithorium-common: Drop SoundTrigger
    * Never ensured it's working in before
    * ST 2.1 interface is deprecated on vintf target-level 6,
      which is the max that our ST HAL provides
    * Let's see if there's any luck of bringing up OSS ST HAL?
    
    Change-Id: I189e0dca38f74be0343c5b345481f3bb7c95bc65

--- mithorium.mk
+++ mithorium.mk
@@ -109,8 +109,7 @@ MITHORIUM_PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.bluetooth.audio-impl
 
 MITHORIUM_PRODUCT_PACKAGES += \
     audio.bluetooth.default \
