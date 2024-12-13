0ae5312e479f3cba6cdbe855e0c2d39307df7151
Yumi Yukimura <me.cafebabe@gmail.com>
2024-12-14 03:56:14 +0800

    [DNM] OpenEUICC
    Change-Id: I14048bdd4336c89e0d2e37bac16133453aa47218

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,7 +29,7 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 # Kernel - Common
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery printk.devkmsg=on
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery printk.devkmsg=on androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0
 ifeq ($(TARGET_BOARD_PLATFORM),msm8937)
     ifeq ($(TARGET_KERNEL_VERSION),4.9)
