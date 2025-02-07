a992c579966c32d4388cf58da3f0306a525ed8f6
Jan Altensen <info@stricted.net>
2025-02-07 21:07:11 +0100

    mithorium-common: move to new fm vendor jni variable
    Change-Id: I48a4c0005a0761ca4c6ff574fca0153d2dbf54c9

--- mithorium.mk
+++ mithorium.mk
@@ -296,6 +296,7 @@ MITHORIUM_PRODUCT_PACKAGES += \
     libfmjni
 
 $(call soong_config_set_bool,libfmjni,no_fm_firmware,true)
+$(call soong_config_set,libfmjni,vendor,qcom)
 
 # Gatekeeper HAL
 ifneq ($(TARGET_USES_DEVICE_SPECIFIC_GATEKEEPER),true)
